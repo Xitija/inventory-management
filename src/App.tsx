e8be762a4445b3a99127c1e878c6742559cb16bc
Kshitija Kadam <kshitijakadam04@gmail.com>
2024-05-15 21:28:44 +0530

    feat : added links to forms and view pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { Route, Link, Routes } from 'react-router-dom';
 import ProductForm from './pages/Productform';
 import './App.css';
+import Products from './pages/Products';
+import SalesForm from './pages/SalesForm';
+import SalesHistory from './pages/SalesHistory';
 
 function App() {
   return (
@@ -9,10 +12,18 @@ function App() {
       {/* <header className="App-header"></header> */}
       <nav>
         <Link to="/add-product">Add Product</Link>
-        {/* <Link to="/exercises">Exercises</Link> */}
+        {'      '}
+        <Link to="/products">Products</Link>
+        {'      '}
+        <Link to="/add-sale">Add Sale</Link>
+        {'      '}
+        <Link to="/sales-history">View Sales History</Link>
       </nav>
       <Routes>
         <Route path="/add-product" element={<ProductForm />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/add-sale" element={<SalesForm />} />
+        <Route path="/sales-history" element={<SalesHistory />} />
       </Routes>
     </div>
   );
