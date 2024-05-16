4c7f7d54ccd2e6564a871debb2f311962e1dbb09
Kshitija Kadam <kshitijakadam04@gmail.com>
2024-05-17 00:13:32 +0530

    feat : added reports and edit form

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import './App.css';
 import Products from './pages/Products';
 import SalesForm from './pages/SalesForm';
 import SalesHistory from './pages/SalesHistory';
+import InventoryReports from './pages/InventoryReports';
+import SalesReports from './pages/SalesReports';
+import EditProduct from './pages/EditProduct';
 
 function App() {
   return (
@@ -13,17 +16,24 @@ function App() {
       <nav>
         <Link to="/add-product">Add Product</Link>
         {'      '}
-        <Link to="/products">Products</Link>
+        <Link to="/products">View Products</Link>
         {'      '}
         <Link to="/add-sale">Add Sale</Link>
         {'      '}
         <Link to="/sales-history">View Sales History</Link>
+        {'      '}
+        <Link to="/inventory-reports">View Inventory reports</Link>
+        {'      '}
+        <Link to="/sales-reports">View Sales reports</Link>
       </nav>
       <Routes>
         <Route path="/add-product" element={<ProductForm />} />
         <Route path="/products" element={<Products />} />
         <Route path="/add-sale" element={<SalesForm />} />
         <Route path="/sales-history" element={<SalesHistory />} />
+        <Route path="/inventory-reports" element={<InventoryReports />} />
+        <Route path="/sales-reports" element={<SalesReports />} />
+        <Route path="/edit-product/:productId" element={<EditProduct />} />
       </Routes>
     </div>
   );
