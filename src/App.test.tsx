d10dbaae6ccd42bf6d3925e0ddf78946acc61450
Kshitija Kadam <65657373+Xitija@users.noreply.github.com>
2025-01-04 17:19:43 +0530

    chore : git reset

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -7,3 +7,8 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+// https://www.ascm.org/globalassets/00_blog/images/22---calculate-manage-inventory-range-main.jpg
+// https://blogimages.softwaresuggest.com/blog/wp-content/uploads/2022/12/19222908/Inventory-Management-Techniques-Methods.png
+// https://blogimages.softwaresuggest.com/blog/wp-content/uploads/2022/12/19222908/Inventory-Management-Techniques-Methods.png
+// https://quickbooks.intuit.com/r/midsize-business/inventory-analysis-methods-strategies-and-procedures/
+// date is to be maintained globally
